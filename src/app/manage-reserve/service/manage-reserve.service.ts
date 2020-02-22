65329887b911cb42d82a08b29f96626509806e81
百科おじさん <tftf161107@gmail.com>
2020-02-22 13:08:02 +0900

    - [add] reserveDataDelete

--- src/app/manage-reserve/service/manage-reserve.service.ts
+++ src/app/manage-reserve/service/manage-reserve.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { map } from 'rxjs/operators'
+import { AngularFirestore } from '@angular/fire/firestore';
 
 import { Reserve } from '../../class/reserve';
 import * as fromReserve from '../store/reserve.reducer';
@@ -10,7 +12,8 @@ import { LoadReserves, AddReserve, DeleteReserve } from '../store/reserve.action
 })
 export class ManageReserveService {
 
-  constructor(private store: Store<fromReserve.State>) {
+  constructor(private store: Store<fromReserve.State>,
+              private db: AngularFirestore) {
     this.store.dispatch(new LoadReserves({ reserves: [] }))
   }
 
@@ -26,6 +29,31 @@ export class ManageReserveService {
     this.store.dispatch(new DeleteReserve({
       id: id
     }))
+    this.reserveDataDelete(id);
+  }
+
+  // reserveを削除する際、それに対応したreserveDataを削除する
+  private reserveDataDelete(reserveId: string) {
+    this.db.collection('communities')
+      .doc('g3Xnp6T1S9xwsDhZLyYZ')
+      .collection('reserveDatas')
+      .snapshotChanges().subscribe(
+        reserveDatas => reserveDatas.map(reserveData => {
+          const data = reserveData.payload.doc.data();
+
+          if (data.reserveId == reserveId) {
+            const id = reserveData.payload.doc.id;
+            this.db.collection('communities')
+              .doc('g3Xnp6T1S9xwsDhZLyYZ')
+              .collection('reserveDatas')
+              .doc(id)
+              .delete()
+              .then(() => console.debug(`${id}:削除完了`))
+              .catch(() => console.debug(`${id}:削除失敗`));
+          }
+
+        })
+      )
   }
 
 }
